6ec100df81da1e8f68c2c2d074c5bcef6db8dc57
codingkompot <ksqasiyah@protonmail.com>
2022-07-25 16:29:39 +0100

    added habit.js and init.js

--- api/server.js
+++ api/server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 
-server.use(cors('*'));
-server.use(express.json());
-
 const connectDB = require("./db/connect");
 const authRouter = require("./routes/auth");
 
