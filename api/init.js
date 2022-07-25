b6454563ab42ca46cdfb32f7ec7a3b54f4fedfd9
codingkompot <ksqasiyah@protonmail.com>
2022-07-25 16:39:08 +0100

    added habit and init.js

---
+++ api/init.js
@@ -0,0 +1,11 @@
+const { MongoClient } = require('mongodb'); //get mongoclient from mongodb library
+
+const connectionUrl = process.env.DB_CONNECTION;
+
+const init = async () => {
+  //async as connecting to an external resource
+  let client = await MongoClient.connect(connectionUrl); //wait for mongo to connect to db
+  return client.db(process.env.DB_NAME);
+};
+
+module.exports = { init };
---
+++ api/models/habit.js
@@ -0,0 +1,78 @@
+const { init } = require("../init.js");
+
+class Habit {
+  constructor(data) {
+    this.username = data.username;
+    this.habit = data.habit;
+    this.frequency = data.frequency;
+    this.status = data.status;
+  }
+  static get all() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const db = await init();
+        const dbData = await db.collection("habitsTracker").find({}).toArray();
+        const allHabits = dbData.map((d) => new Habit(d));
+        if (!allHabits.length) {
+          throw new Error(
+            "You don't have any habits that you are tracking at the moment!"
+          );
+        }
+        resolve(allHabits);
+      } catch (err) {
+        reject(`Error retrieving habits: ${err.message}`);
+      }
+    });
+  }
+  static create(habit) {
+    return new Promise(async (res, rej) => {
+      try {
+        const db = await init();
+        const createHabit = await db
+          .collection("habitsTracker")
+          .findOneAndUpdate(
+            { username: habit.username },
+            { $push: { habit: habit.habit } }
+          );
+        res(createHabit);
+      } catch (err) {
+        rej(`Error creating habits: ${err}`);
+      }
+    });
+  }
+
+  static findByUsername(username) {
+    return new Promise(async (res, rej) => {
+      try {
+        const db = await init();
+        const user = await db
+          .collection("habitsTracker")
+          .findOne({ username: username });
+        res(user);
+      } catch (err) {
+        rej(`Error retrieving habits: ${err}`);
+      }
+    });
+  }
+  static get findHabitByUsername() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const db = await init();
+        const dbData = await db
+          .collection("habitsTracker")
+          .find({ username: habit.username });
+        const findUserHabits = dbData.map((d) => new Habit(d));
+        if (!findUserHabits.length) {
+          throw new Error(
+            "You don't have any habits that you are tracking at the moment!"
+          );
+        }
+        resolve(findUserHabits);
+      } catch (err) {
+        reject(`Error retrieving habits: ${err.message}`);
+      }
+    });
+  }
+}
+
+module.exports = Habit;
