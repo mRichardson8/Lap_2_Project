144def1a6398f1951c8ee7d67fc323431ef5a90a
codingkompot <ksqasiyah@protonmail.com>
2022-07-25 14:50:03 +0100

    updated register function in controllers/auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,8 +1,13 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
   try {
-    const user = await User.create({ ...req.body });
+    const { name, email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(password, salt);
+    const tempUser = { name, email, password: hashed };
+    const user = await User.create({ ...tempUser });
     res.status(201).json({ user });
   } catch (error) {
     res.status(500).json({ msg: error });
