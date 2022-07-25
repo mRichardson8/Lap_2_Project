c17ee8b16bccd6dfb425faa87a3657d7d287c82b
jamesthompson02 <jamesthompsongithub@outlook.com>
2022-07-25 17:38:59 +0100

    Fixed yet another conflict issue

--- api/models/habit.js
+++ api/models/habit.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
-=======
-const { connectDB } = require("../db/connect");
->>>>>>> a0ab063dec881ae70cb32fecbab7e177cacb9649
 
 const HabitSchema = new mongoose.Schema({
   name:{
@@ -13,64 +9,14 @@ const HabitSchema = new mongoose.Schema({
   },
   habits: {
     type: Array, 
-    
-  }
-<<<<<<< HEAD
+    required: [true, "Please choose at least one habit!"]
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "Please provide user"],
 
-=======
-  static delete(habit) {
-  return new Promise(async (res, rej) => {
-    try {
-        const db = await init();
-        const createHabit = await db
-          .collection("habitsTracker")
-          .findOneAndDelete(
-            { username: habit.username },
-            { $push: { habit: habit.habit } }
-          );
-        res(createHabit);
-      } catch (err) {
-        rej(`Error deleting habits: ${err}`);
-      }
-  })
-}
-  static findByUsername(username) {
-    return new Promise(async (res, rej) => {
-      try {
-        const db = await init();
-        const user = await db
-          .collection("habitsTracker")
-          .findOne({ username: username });
-        res(user);
-      } catch (err) {
-        rej(`Error retrieving habits: ${err}`);
-      }
-    });
   }
-  static get findHabitByUsername() {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const db = await init();
-        const dbData = await db
-          .collection("habitsTracker")
-          .find({ username: habit.username });
-        const findUserHabits = dbData.map((d) => new Habit(d));
-        if (!findUserHabits.length) {
-          throw new Error(
-            "You don't have any habits that you are tracking at the moment!"
-          );
-        }
-        resolve(findUserHabits);
-      } catch (err) {
-        reject(`Error retrieving habits: ${err.message}`);
-      }
-    });
-  }
-}
->>>>>>> a0ab063dec881ae70cb32fecbab7e177cacb9649
-
-
-    
 })
 
 const habit = db.model('Habit', HabitSchema);
