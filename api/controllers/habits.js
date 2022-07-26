6660a3f10ff3e5c056582c577df4da074702970b
codingkompot <ksqasiyah@protonmail.com>
2022-07-26 10:32:31 +0100

    fixed controllers/habits.js functions

--- api/controllers/habits.js
+++ api/controllers/habits.js
@@ -1,27 +1,27 @@
 const Habit = require('../models/habit');
 
-HabitSchema.statics.getHabits = function(req, res) {
-    return new Promise((resolve, reject) => {
-        this.find((error, docs) => {
-            if (error) {
-                console.error(error);
-                return reject(error);
-            }
-            resolve(docs);
-        })
-    })
-}
+// HabitSchema.statics.getHabits = function(req, res) {
+//     return new Promise((resolve, reject) => {
+//         this.find((error, docs) => {
+//             if (error) {
+//                 console.error(error);
+//                 return reject(error);
+//             }
+//             resolve(docs);
+//         })
+//     })
+// }
 
-HabitSchema.statics.getHabitsByName = function(req, res) {
+const getHabitsByName = async (req, res) => {
     try {
         const habit = await Habit.findById(req.params.id);
         res.status(200).json({ habit });
       } catch (err) {
         res.status(500).send(err);
       }
-}
+};
 
-HabitSchema.statics.create = function(req, res) {
+const createHabit = async (req, res) => {
     try {
          const habit = await Habit.create(req.body);
          res.status(201).json(habit);
@@ -31,7 +31,7 @@ HabitSchema.statics.create = function(req, res) {
      }
 }
 
-HabitSchema.statics.delete = function(req, res) {
+const deleteHabit = async (req, res) =>{
     try {
         const habit = await Habit.findOneAndDelete(req.body);
         res.status(200).json(habit);
@@ -40,3 +40,9 @@ HabitSchema.statics.delete = function(req, res) {
         res.status(422).json({ message: err });
     }
 }
+
+module.exports = {
+    getHabitsByName,
+    createHabit,
+    deleteHabit,
+  };
