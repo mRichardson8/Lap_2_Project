35abb911e8754c79939157aa78b614df7e078256
nouria chaouy <nouria.chaouy@gmail.com>
2022-07-26 10:52:25 +0100

    updated habit schema

--- api/models/habit.js
+++ api/models/habit.js
@@ -5,18 +5,19 @@ const HabitSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide a name"],
     minlength: 3,
-    maxlength: 50
+    maxlength: 50,
   },
   habits: {
-    type: Array, 
-    required: [true, "Please choose at least one habit!"]
+    required: [true, "Please choose at least one habit!"],
   },
   createdBy: {
     type: mongoose.Types.ObjectId,
     ref: "User",
     required: [true, "Please provide user"],
+  },
+  data: {
+    type: Array,
+  },
+});
 
-  }
-})
-
-const habit = db.model('Habit', HabitSchema);
+module.exports = mongoose.model("Habit", HabitSchema);
