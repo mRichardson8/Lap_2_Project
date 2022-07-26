fbc5d6f04591025909859178f0b2e00c7139ae5f
codingkompot <ksqasiyah@protonmail.com>
2022-07-26 15:26:25 +0100

    added update function and route

--- api/controllers/habits.js
+++ api/controllers/habits.js
@@ -1,60 +1,47 @@
-const Habit = require('../models/habit');
-
-// HabitSchema.statics.getHabits = function(req, res) {
-//     return new Promise((resolve, reject) => {
-//         this.find((error, docs) => {
-//             if (error) {
-//                 console.error(error);
-//                 return reject(error);
-//             }
-//             resolve(docs);
-//         })
-//     })
-// }
+const Habit = require("../models/habit");
 
 const getHabitsByName = async (req, res) => {
-    try {
-
-        const habit = await Habit.findOne({createdBy : req.user.userId});
-        res.status(200).json({ habit });
-      } catch (err) {
-        res.status(500).send(err);
-      }
+  try {
+    const habit = await Habit.findOne({ createdBy: req.user.userId });
+    res.status(200).json({ habit });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 const createHabit = async (req, res) => {
-    try {
-          req.body.createdBy = req.user.userId;
-         const habit = await Habit.create(req.body);
-         res.status(201).json(habit);
-     } catch (err) {
-        console.log(err);
-         res.status(422).json({ message: err });
-     }
-}
+  try {
+    req.body.createdBy = req.user.userId;
+    const habit = await Habit.create(req.body);
+    res.status(201).json(habit);
+  } catch (err) {
+    console.log(err);
+    res.status(422).json({ message: err });
+  }
+};
 
 const updateHabit = async (req, res) => {
-    try {
-        const habit = await Habit.findOneAndUpdate(req.body);
-        res.status(200).json({ habit });
-      } catch (err) {
-        res.status(417).send(err);
-      }
+  try {
+    const habit = await Habit.findOneAndUpdate({ createdBy: req.user.userId }, req.body, {new: true, runValidators: true});
+    res.status(200).json({ habit });
+  } catch (err) {
+    res.status(417).send(err);
+  }
 };
 
 const deleteHabit = async (req, res) => {
-    try {
-        const habit = await Habit.findOneAndDelete(req.body);
-        res.status(200).json(habit);
-    } catch (err) {
-       console.log(err);
-        res.status(422).json({ message: err });
-    }
-}
+  try {
+    const habit = await Habit.findOneAndDelete(req.body);
+    res.status(200).json(habit);
+  } catch (err) {
+    console.log(err);
+    res.status(422).json({ message: err });
+  }
+};
 
 module.exports = {
-    getHabitsByName,
-    createHabit,
-    updateHabit,
-    deleteHabit,
-  };
+  getHabitsByName,
+  createHabit,
+  updateHabit,
+  deleteHabit,
+};
