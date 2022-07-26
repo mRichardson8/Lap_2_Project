144030e2ff4724241269e83480b5554ecc870ec5
nouria chaouy <nouria.chaouy@gmail.com>
2022-07-26 10:57:21 +0100

    added authentication middleware to habit routes

--- api/routes/habit.js
+++ api/routes/habit.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const authentication = require("../middleware/authentication");
 
-const { getHabitsByName, createHabit, deleteHabit } = require("../controllers/habits");
+const {
+  getHabitsByName,
+  createHabit,
+  deleteHabit,
+} = require("../controllers/habits");
 
-router.post("/habits", getHabitsByName);
-router.post("/createhabit", createHabit);
-router.post("/deletehabit", deleteHabit);
+router.post("/habits", authentication, getHabitsByName);
+router.post("/createhabit", authentication, createHabit);
+router.post("/deletehabit", authentication, deleteHabit);
 
 module.exports = router;
