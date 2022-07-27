ecea51d59084b0dc08aaef60e7d7b06fe4d889c8
nouria chaouy <nouria.chaouy@gmail.com>
2022-07-27 17:04:10 +0100

    updated the charts with some information

--- client/static/js/userPage.js
+++ client/static/js/userPage.js
@@ -1,164 +1,232 @@
+const data = [
+  {
+    date: "Tue",
+    met: ["water", "exercise"],
+    habits: {
+      water: { current: 2000, required: 2000, met: true, streak: 5 },
+      exercise: { current: 60, required: 30, met: true, streak: 2 },
+    },
+  },
+  {
+    date: "Wed",
+    met: ["water", "exercise"],
+    habits: {
+      water: { current: 2000, required: 2000, met: true, streak: 5 },
+      exercise: { current: 60, required: 30, met: true, streak: 2 },
+    },
+  },
+  {
+    date: "Thu",
+    met: ["water", "exercise"],
+    habits: {
+      water: { current: 2000, required: 2000, met: true, streak: 5 },
+      exercise: { current: 60, required: 30, met: true, streak: 2 },
+    },
+  },
+  {
+    date: "Fri",
+    met: ["water", "exercise"],
+    habits: {
+      water: { current: 2000, required: 2000, met: true, streak: 5 },
+      exercise: { current: 60, required: 30, met: true, streak: 2 },
+    },
+  },
+];
+
+createChart(data);
+
 async function getUserData() {
+  let response = await fetch("https://viva-pal.herokuapp.com/api/habits", {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + localStorage.token,
+    },
+  });
+  console.log(response);
+  let data = await response.json();
+  console.log(data);
 
-    let response = await fetch('https://viva-pal.herokuapp.com/api/habits', {
-        method: "GET",
-        headers: {
-             "Authorization": "Bearer " + localStorage.token,
-        }
-    })
-    console.log(response)
+  if (!data) {
+    document.getElementById("user-page-create-habits-container").style.display =
+      "flex";
+  } else {
+    console.log(data.habits);
+    createUserDetails(data.name);
+    createHabits(data.habits);
+  }
+}
+
+async function sendHabits() {
+  let habits = {};
+  if (document.getElementById("exercise-check").checked) {
+    habits.exercise = {
+      required: document.getElementById("exercise-goal").value,
+      streak: 0,
+      current: 0,
+      met: false,
+    };
+  }
+  if (document.getElementById("water-check").checked) {
+    habits.water = {
+      required: document.getElementById("water-goal").value,
+      streak: 0,
+      current: 0,
+      met: false,
+    };
+  }
+  if (document.getElementById("sleep-check").checked) {
+    habits.sleep = {
+      required: document.getElementById("sleep-goal").value,
+      streak: 0,
+      current: 0,
+      met: false,
+    };
+  }
+  let habitObj = {
+    name: localStorage.name,
+    createdBy: localStorage.userId,
+    habits: habits,
+    data: [],
+  };
+
+  let response = await fetch("https://viva-pal.herokuapp.com/api/createhabit", {
+    method: "POST",
+    headers: {
+      authorization: "Bearer " + localStorage.token,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(habitObj),
+  });
+
+  console.log(response);
+
+  if (response.status === 201) {
     let data = await response.json();
-    console.log(data)
-    
-    if (!data) {
-        document.getElementById('user-page-create-habits-container').style.display = 'flex';
-    }else{
-        console.log(data.habits);
-        createUserDetails(data.name);
-        createHabits(data.habits);
-    }
+    console.log(data);
+    createUserDetails(data.name);
+    createHabits(data.habits);
+    document.getElementById("user-page-create-habits-container").style.display =
+      "none";
+  }
 }
 
-async function sendHabits(){
-    let habits = {}
-    if (document.getElementById('exercise-check').checked){
-        habits.exercise = { required : document.getElementById('exercise-goal').value,streak:0,current:0,met:false}
-    };
-    if (document.getElementById('water-check').checked){
-        habits.water = { required : document.getElementById('water-goal').value,streak:0,current:0,met:false}
-    };
-    if (document.getElementById('sleep-check').checked){
-        habits.sleep = { required : document.getElementById('sleep-goal').value,streak:0,current:0,met:false}
-    };
-    let habitObj = {
-        name: localStorage.name,
-        createdBy: localStorage.userId,
-        habits : habits,
-        data : []
-    }
-
-    let response = await fetch('https://viva-pal.herokuapp.com/api/createhabit', {
-        method: 'POST',
-        headers: {
-            "authorization": "Bearer " + localStorage.token,
-            'Content-Type': 'application/json'
+async function updateHabits() {
+  let data = await fetch("https://viva-pal.herokuapp.com/api/habits", {
+    method: "PATCH",
+    headers: {
+      Authorization: "Bearer " + localStorage.token,
+    },
+    body: JSON.stringify({
+      //make function to create habits object to send with only right habits
+      habits: {
+        //TODO this needs to be changed
+        water: {
+          current: parseInt(
+            document
+              .querySelector("#water-container .habit-current")
+              .innerText.split(" ")[3]
+          ),
         },
-        body: JSON.stringify(habitObj)
-    })
-
-    console.log(response);
-    
-    if (response.status === 201) {
-        let data = await response.json();
-        console.log(data);
-        createUserDetails(data.name);
-        createHabits(data.habits);
-        document.getElementById('user-page-create-habits-container').style.display = "none";
-        
-    }
-}
-
-async function updateHabits(){
-    let data = await fetch('https://viva-pal.herokuapp.com/api/habits', {
-        method: "PATCH",
-        headers: {
-             "Authorization": "Bearer " + localStorage.token,
+        exercise: {
+          current: parseInt(
+            document
+              .querySelector("#exercise-container .exercise-current")
+              .innerText.split(" ")[3]
+          ),
         },
-        body: JSON.stringify({ //make function to create habits object to send with only right habits
-            habits : { //TODO this needs to be changed
-                water : {current: parseInt(document.querySelector('#water-container .habit-current').innerText.split(' ')[3])},
-                exercise : {current: parseInt(document.querySelector('#exercise-container .exercise-current').innerText.split(' ')[3])},
-                sleep: {current: parseInt(document.querySelector('#sleep-container .sleep-current').innerText.split(' ')[4])},
-            }
-        })
-    })
-    let response = await data.json();
-    if (!response.habit) {
-        document.getElementById('user-page-create-habits-container').style.display = 'flex';
-    }else{
-        createUserDetails(response.name);
-        createHabits(response.habits);
-    }
+        sleep: {
+          current: parseInt(
+            document
+              .querySelector("#sleep-container .sleep-current")
+              .innerText.split(" ")[4]
+          ),
+        },
+      },
+    }),
+  });
+  let response = await data.json();
+  if (!response.habit) {
+    document.getElementById("user-page-create-habits-container").style.display =
+      "flex";
+  } else {
+    createUserDetails(response.name);
+    createHabits(response.habits);
+  }
 }
-
 
 let testHabits = {
-    createdBy:"62dfcb97433fa6ec72a53fc6",
-     data:[{}],
-     habits : {
-         exercise:{required:30,streak:5, met: false, current: 0},
-         water:{required:2000,streak:2, met: true, current: 2500},
-         sleep:{required:8,streak:0, met: false, current: 0}
-     },
-     name: "Matthew Richardson",
- }
+  createdBy: "62dfcb97433fa6ec72a53fc6",
+  data: [{}],
+  habits: {
+    exercise: { required: 30, streak: 5, met: false, current: 0 },
+    water: { required: 2000, streak: 2, met: true, current: 2500 },
+    sleep: { required: 8, streak: 0, met: false, current: 0 },
+  },
+  name: "Matthew Richardson",
+};
 
-createUserDetails(testHabits.name)
-createHabits(testHabits.habits)
+createUserDetails(testHabits.name);
+createHabits(testHabits.habits);
 // getUserData();
 // document.getElementById('user-page-create-habits-container').style.display = 'flex';
 
 // This first block of code relates to the user being able to progress through the sign
 // up process and also go back to previous sections.
 
-
-const habits1 = [...document.querySelectorAll('.user-page-checkbox')];
-const rightArrow1 = document.getElementById('right-arrow-1');
-const nextButton1 = document.getElementById('user-page-first-next-button');
-const leftArrow1 = document.getElementById('left-arrow-1');
-const userPageSections = [...document.querySelectorAll('.user-page-section')];
-const userPageSelectContainers = [...document.querySelectorAll('.user-page-select-container')];
-const nextButtons = [...document.querySelectorAll('.user-page-next-button')];
-const userPageSubmitBtn = document.getElementById('user-page-submit-button');
+const habits1 = [...document.querySelectorAll(".user-page-checkbox")];
+const rightArrow1 = document.getElementById("right-arrow-1");
+const nextButton1 = document.getElementById("user-page-first-next-button");
+const leftArrow1 = document.getElementById("left-arrow-1");
+const userPageSections = [...document.querySelectorAll(".user-page-section")];
+const userPageSelectContainers = [
+  ...document.querySelectorAll(".user-page-select-container"),
+];
+const nextButtons = [...document.querySelectorAll(".user-page-next-button")];
+const userPageSubmitBtn = document.getElementById("user-page-submit-button");
 
 function nextSection() {
-    let checkCounter = 0;
+  let checkCounter = 0;
 
-    for (let each of userPageSelectContainers) {
-        each.style.display = 'none';
+  for (let each of userPageSelectContainers) {
+    each.style.display = "none";
+  }
+
+  for (let each of habits1) {
+    if (each.checked === true) {
+      checkCounter += 1;
     }
-
+  }
+  if (checkCounter > 0) {
     for (let each of habits1) {
-        if (each.checked === true) {
-            checkCounter += 1;
-        }
+      if (each.checked === true) {
+        let indexPosition = habits1.indexOf(each);
+        userPageSelectContainers[indexPosition].style.display = "flex";
+      }
     }
-    if (checkCounter > 0) {
-        for (let each of habits1) {
-            if (each.checked === true) {
-                let indexPosition = habits1.indexOf(each);
-                userPageSelectContainers[indexPosition].style.display = 'flex';
-            }
-        }
-        document.getElementById('user-page-first').style.display = 'none';
-        document.getElementById('user-page-second').style.display = 'flex';
-    } else {
-        alert('Choose at least one habit to move to next section');
-    }
-
+    document.getElementById("user-page-first").style.display = "none";
+    document.getElementById("user-page-second").style.display = "flex";
+  } else {
+    alert("Choose at least one habit to move to next section");
+  }
 }
 
 function previousSection() {
-    document.getElementById('user-page-second').style.display = 'none';
-    document.getElementById('user-page-first').style.display = 'flex';
+  document.getElementById("user-page-second").style.display = "none";
+  document.getElementById("user-page-first").style.display = "flex";
 }
 
+rightArrow1.addEventListener("click", () => {
+  nextSection();
+});
 
-rightArrow1.addEventListener('click', () => {
-    nextSection();
-})
+nextButton1.addEventListener("click", () => {
+  nextSection();
+});
 
-nextButton1.addEventListener('click', () => {
-    nextSection();
-})
+leftArrow1.addEventListener("click", () => {
+  previousSection();
+});
 
-leftArrow1.addEventListener('click', () => {
-    previousSection();
-})
-
-userPageSubmitBtn.addEventListener('click', () => {
-    sendHabits();
-    
-})
-
-
+userPageSubmitBtn.addEventListener("click", () => {
+  sendHabits();
+});
--- client/static/js/userSetup.js
+++ client/static/js/userSetup.js
@@ -9,12 +9,12 @@ function createUserDetails(user) {
   settingsIcon.setAttribute("class", "icons");
   let settingsIcon2 = document.createElement("p");
   settingsIcon2.innerHTML =
-  '<span class="material-symbols-outlined"> dark_mode </span>';
+    '<span class="material-symbols-outlined"> dark_mode </span>';
   settingsIcon2.setAttribute("class", "icons2");
   let name = document.createElement("p");
   name.innerText = user;
   name.setAttribute("class", "userName");
-  userNav.append(title, settingsIcon, settingsIcon2, name,);
+  userNav.append(title, settingsIcon, settingsIcon2, name);
   dashboard.prepend(userNav);
 }
 function createHabits(habits) {
@@ -33,55 +33,59 @@ function createHabits(habits) {
   }
 }
 
-function createWaterDiv(data){
-    let div = document.createElement('div');
-    div.setAttribute('id', 'water-container');
-    div.setAttribute('class', 'habit-container');
-    let title = document.createElement('h3');
-    title.innerText = "Water Intake";
-    let streak = document.createElement('p');
-    streak.innerText = "Streak : " + data.streak;
-    streak.setAttribute('class', 'habit-streak');
-    let target = document.createElement('p');
-    target.innerText = "Target : " + data.required + " ml";
-    target.setAttribute('class', 'habit-target');
-    let current = document.createElement('p');
-    current.innerText = "Water drank today: " + data.current + " ml"; 
-    current.setAttribute('class', 'habit-current');
-    current.setAttribute('id', 'water-current')
-    let addBtn = document.createElement('button');
-    addBtn.innerText = "+";
-    addBtn.addEventListener('click', () => {
-        let inputDiv = document.createElement('div');
-        inputDiv.style.display = 'flex';
-        inputDiv.style.flexDirection = 'column';
-        let inputContainer = document.createElement('div');
-        inputContainer.style.display = 'flex';
-        let btnsContainer = document.createElement('div');
-        btnsContainer.style.display = 'flex';
-        let inputLabel = document.createElement('label');
-        inputLabel.textContent = 'Water consumed:';
-        let inputNumber = document.createElement('input');
-        inputNumber.setAttribute('type', 'number');
-        let inputSubmitBtn = document.createElement('button');
-        inputSubmitBtn.textContent = "Add entry";
-        inputSubmitBtn.addEventListener('click', () => {
-            inputDiv.remove();
-        })
-        let inputUndoBtn = document.createElement('button');
-        inputUndoBtn.textContent = 'Undo';
-        inputUndoBtn.addEventListener('click', () => {
-            inputDiv.remove();
-        })
-        inputContainer.appendChild(inputLabel);
-        inputContainer.appendChild(inputNumber);
-        btnsContainer.appendChild(inputUndoBtn);
-        btnsContainer.appendChild(inputSubmitBtn);
-        inputDiv.appendChild(inputContainer);
-        inputDiv.appendChild(btnsContainer);
-        document.getElementById('water-current').parentNode
-        .insertBefore(inputDiv, document.getElementById('water-current').nextSibling);
-    })
+function createWaterDiv(data) {
+  let div = document.createElement("div");
+  div.setAttribute("id", "water-container");
+  div.setAttribute("class", "habit-container");
+  let title = document.createElement("h3");
+  title.innerText = "Water Intake";
+  let streak = document.createElement("p");
+  streak.innerText = "Streak : " + data.streak;
+  streak.setAttribute("class", "habit-streak");
+  let target = document.createElement("p");
+  target.innerText = "Target : " + data.required + " ml";
+  target.setAttribute("class", "habit-target");
+  let current = document.createElement("p");
+  current.innerText = "Water drank today: " + data.current + " ml";
+  current.setAttribute("class", "habit-current");
+  current.setAttribute("id", "water-current");
+  let addBtn = document.createElement("button");
+  addBtn.innerText = "+";
+  addBtn.addEventListener("click", () => {
+    let inputDiv = document.createElement("div");
+    inputDiv.style.display = "flex";
+    inputDiv.style.flexDirection = "column";
+    let inputContainer = document.createElement("div");
+    inputContainer.style.display = "flex";
+    let btnsContainer = document.createElement("div");
+    btnsContainer.style.display = "flex";
+    let inputLabel = document.createElement("label");
+    inputLabel.textContent = "Water consumed:";
+    let inputNumber = document.createElement("input");
+    inputNumber.setAttribute("type", "number");
+    let inputSubmitBtn = document.createElement("button");
+    inputSubmitBtn.textContent = "Add entry";
+    inputSubmitBtn.addEventListener("click", () => {
+      inputDiv.remove();
+    });
+    let inputUndoBtn = document.createElement("button");
+    inputUndoBtn.textContent = "Undo";
+    inputUndoBtn.addEventListener("click", () => {
+      inputDiv.remove();
+    });
+    inputContainer.appendChild(inputLabel);
+    inputContainer.appendChild(inputNumber);
+    btnsContainer.appendChild(inputUndoBtn);
+    btnsContainer.appendChild(inputSubmitBtn);
+    inputDiv.appendChild(inputContainer);
+    inputDiv.appendChild(btnsContainer);
+    document
+      .getElementById("water-current")
+      .parentNode.insertBefore(
+        inputDiv,
+        document.getElementById("water-current").nextSibling
+      );
+  });
 
   div.append(title, streak, target, current, addBtn);
   return div;
@@ -131,27 +135,27 @@ function createSleepDiv(data) {
   return div;
 }
 
-const ctx = document.getElementById("myChart").getContext("2d");
-
-function createChart(habitData) {
+function createChart(dataHabit) {
+  const ctx = document.getElementById("myChart").getContext("2d");
   // what goes on the x axis
-  const labels = [];
+  const labels = dataHabit.map((d) => d.date);
+  //   const goals = Object.keys(dataHabit).length;
   const data = {
     labels,
     datasets: [
       {
-        label: 'Goals',
-        backgroundColor: "red",
+        label: "Water",
+        backgroundColor: "#3decdd",
         borderColor: "rgb(255, 99, 132)",
         // points on the bar chart where we put the actual data we need
-        data: habitData,
+        data: [20, 30, 45],
       },
       {
-        label: "Accomplished",
+        label: "Sleep",
         backgroundColor: "blue",
         borderColor: "rgb(255, 99, 132)",
         // points on the bar chart where we put the actual data we need,
-        data: habitData,
+        data: [20, 30, 45].reverse(),
       },
     ],
   };
@@ -159,8 +163,17 @@ function createChart(habitData) {
     type: "bar",
     data: data,
     options: {
-      responsive: true,
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+        },
+      },
     },
   };
   const myChart = new Chart(ctx, config);
 }
+
+// createChart();
