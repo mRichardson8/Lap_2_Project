6db715c3024c940d5c35ea33bb02544b7995eaed
jamesthompson02 <jamesthompsongithub@outlook.com>
2022-07-28 15:45:48 +0100

    Oi oi

--- client/static/js/userSetup.js
+++ client/static/js/userSetup.js
@@ -85,7 +85,7 @@ function createWaterDiv(data) {
   addBtn.addEventListener("click", (e) => {
     if (e.target === document.getElementById("water-current").nextSibling) {
       let inputDiv = document.createElement("div");
-      inputDiv.setAttribute('class', "water-input-container");
+      inputDiv.setAttribute('class', "water-input-container");//
       inputDiv.style.display = "flex";
       inputDiv.style.flexDirection = "column";
       let inputContainer = document.createElement("div");
