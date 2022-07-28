575b2487302615cf75cfc00083190f9eed5ad49a
Matthew Richardson <matt.rich2045@gmail.com>
2022-07-28 17:50:04 +0100

    changed graph color

--- client/static/js/userSetup.js
+++ client/static/js/userSetup.js
@@ -472,7 +472,7 @@ function calcDatasets(habits) {
   const values = habits.map((habits) =>
     calcValues(Object.entries(habits.habits))
   );
-  let backgrounds = ["#3decdd", "blue", "red"];
+  let backgrounds = ["#3decdd", "blue", "#49799b"];
   let borders = ["rgb(255, 99, 132)", "rgb(255, 99, 132)", "rgb(255, 99, 132)"];
   for (let count = 0; count < numberOfHabits; count++) {
     dataset.push({
