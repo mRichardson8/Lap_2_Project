68192e583bd25c118b687439ef19ddf88f8ca73e
Matthew Richardson <matt.rich2045@gmail.com>
2022-07-29 08:59:12 +0100

    added test for login in auth controller

--- api/__tests__/unit/user.controller.test.js
+++ api/__tests__/unit/user.controller.test.js
@@ -1,6 +1,7 @@
 const authController = require('../../controllers/auth')
 const User = require('../../models/user')
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs')
 
 const mockSend = jest.fn();
 const mockJson = jest.fn();
@@ -25,6 +26,32 @@ describe('auth controller', () => {
                   const mockReq = { body: testUser }
                   await authController.register(mockReq, mockRes);
                   expect(mockStatus).toHaveBeenCalledWith(201);
+                  expect(mockJson).toHaveBeenCalledWith({token});
+        })
+    }),
+    describe('login', () => {
+        test('it logins an existing user and returns a JWT token with a status of 200', async () => {
+            let testUser = {
+                email: "name@email.com",
+                password: "Secret23!",
+              }
+              const salt = await bcrypt.genSalt(10);
+              const testPassword = await bcrypt.hash(testUser.password, salt);
+            let returnedUser = {
+                email: "name@email.com",
+                password: testPassword,
+            }
+              let token = "token"
+            testUser.createJWT = jest.fn(() => token)
+            testUser.comparePassword = jest.fn(async (password)=>{
+                const isMatch = await bcrypt.compare(password, testPassword);
+                return isMatch;
+            })
+            jest.spyOn(User, 'findOne')
+                 .mockResolvedValue(testUser);
+                  const mockReq = { body: testUser }
+                  await authController.login(mockReq, mockRes);
+                  expect(mockStatus).toHaveBeenCalledWith(200);
                   expect(mockJson).toHaveBeenCalledWith({token});
         })
     })
